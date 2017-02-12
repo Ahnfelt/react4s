7cfcdb01ef535811e8267068db790eeab9596f83
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2017-02-12 23:18:42 +0100

    Cleaned up the example

--- src/main/scala/dk/ahnfelt/react4s/Main.scala
+++ src/main/scala/dk/ahnfelt/react4s/Main.scala
@@ -10,7 +10,6 @@ package dk.ahnfelt.react4s
 
 
 import scala.scalajs.js
-import dk.ahnfelt.react4s.FancyButton._
 
 
 object Main extends js.JSApp {
@@ -20,138 +19,19 @@ object Main extends js.JSApp {
     }
 }
 
-case class EchoMyNameWithState() extends Component[Unit] {
-    val name = State("John Doe")
-    override def render() : Element = {
-        E.div(
-            E.input(H.bind(name)),
-            E.div(H.text(name()), Colorful),
-            E.hr(),
-            H(Echo, name(), name.set)
-        )
-    }
-}
-
-case class EchoMyName() extends Component[Unit] {
-    var name : String = ""
-    override def render() : Element = {
-        E.div(
-            E.input(H.bindVar(name, {name = _}, update)),
-            E.div(H.text(name)),
-            E.hr(),
-            E.div(
-                H(Echo, name, { newName : String => name = newName; update() })
-            )
-        )
-    }
-}
-
-case class Echo(name : P[String]) extends Component[String] {
-    override def render() : Element = {
-        E.div(
-            E.input(H.bindProp(name(), emit)),
-            E.div(H.text(name()))
-        )
-    }
-}
-
-object Colorful extends CssClass(
-    S.color.rgb(200, 0, 0),
-    S.cursor.pointer(),
-    Css.mediaLarge(
-        Css.hover(
-            S.textDecoration("underline")
-        )
-    )
-)
-
-
-
-case class Baz(list : List[String])
-
-case class Outer() extends Component[Unit] {
-    var baz = Baz(List("First", "Second"))
-
-    def onTimeout() = {
-        baz = Baz(List("First", "Second"))
-        update()
-    }
-
-    js.Dynamic.global.setTimeout(() => onTimeout(), 1000)
-
-    override def render() = {
-        println("Render outer")
-        E.div(
-            S.display.block(),
-            S.color.rgb(50, 50, 50),
-            H(Inner, baz, println)
-        )
-    }
-}
-
-case class Inner(labels : P[Baz]) extends Component[String] {
-    override def render() = {
-        println("Render inner")
-        E.div(
-            H(ButtonList, labels().list, emit)
-        )
-    }
-}
-
-
-
-case class FancyButton(label : P[String]) extends Component[FancyButton.Message] {
-    override def render() = {
-        E.div(
-            A.onClick(_ => emit(Click)),
-            H.text(label())
-        )
-    }
-}
-
-object FancyButton {
-    sealed abstract class Message
-    case object Click extends Message
-}
-
-
-case class ButtonPanel() extends Component[Unit] {
-
-    def onClick(message : FancyButton.Message) = message match {
-        case FancyButton.Click =>
-            println("Button clicked!")
-    }
-
-    override def render() = {
-        E.div(
-            H(FancyButton, "Click me!", onClick)
-        )
-    }
-}
-
-case class ButtonList(labels : P[List[String]]) extends Component[String] {
-
-    def onClick(label : String)(message : FancyButton.Message) = emit(label)
-
-    override def render() = {
-        E.div(
-            H.list(labels().map { label =>
-                H(FancyButton, label, onClick(label)).withKey(label)
-            })
-        )
-    }
-}
-
 
 
 // List of removable counters example
 
+
 case class Counter(value : Int)
+
 
 sealed abstract class CounterMessage
 case object Increment extends CounterMessage
 case object Decrement extends CounterMessage
 case object Remove extends CounterMessage
+
 
 case class CounterListComponent() extends Component[Unit] {
 
@@ -174,7 +54,7 @@ case class CounterListComponent() extends Component[Unit] {
 
     override def render() = {
         E.div(
-            E.button(H.text("Add"), A.onClick(_ => onAddCounter())),
+            E.button(H.text("Add"), A.onClick(_ => onAddCounter()), FancyButtonCss),
             H.list(
                 counters().zipWithIndex.map { case (counter, index) =>
                     H(CounterComponent, counter, onCounterMessage(index)).withKey(index.toString)
@@ -184,20 +64,33 @@ case class CounterListComponent() extends Component[Unit] {
     }
 }
 
+
 case class CounterComponent(counter : P[Counter]) extends Component[CounterMessage] {
 
     def spacer(width : Int) = E.span(S.display.inlineBlock(), S.width.px(width))
 
     override def render() = {
         E.div(
-            E.button(H.text("-"), A.onClick(_ => emit(Decrement))),
+            E.button(H.text("-"), A.onClick(_ => emit(Decrement)), FancyButtonCss),
             spacer(20),
             H.text(counter().value.toString),
             spacer(20),
-            E.button(H.text("+"), A.onClick(_ => emit(Increment))),
+            E.button(H.text("+"), A.onClick(_ => emit(Increment)), FancyButtonCss),
             spacer(50),
-            E.button(H.text("X"), A.onClick(_ => emit(Remove)))
+            E.button(H.text("X"), A.onClick(_ => emit(Remove)), FancyButtonCss)
         )
     }
 
 }
+
+
+object FancyButtonCss extends CssClass(
+    S.cursor.pointer(),
+    S.border.px(2).solid().rgb(0, 0, 0),
+    S.color.rgb(0, 0, 0),
+    S.backgroundColor.rgb(255, 255, 255),
+    Css.hover(
+        S.color.rgb(255, 255, 255),
+        S.backgroundColor.rgb(0, 0, 0)
+    )
+)
