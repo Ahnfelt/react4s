bca3db89dcd0dffd820fd2bfd6a7abfd2092af51
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-05-29 18:55:25 +0200

    0.9.14: Lazy Tags.when and Tags(...) with varargs.

--- src/main/scala/com/github/ahnfelt/react4s/Component.scala
+++ src/main/scala/com/github/ahnfelt/react4s/Component.scala
@@ -207,8 +207,12 @@ final case class Element(tagName : String, children : Seq[Tag], key : Option[Str
 /** A list of tags. The list will be flattened into the parent before the Virtual DOM is reconciled. */
 case class Tags(tags : Seq[Tag]) extends Tag
 object Tags {
+    /** Varargs tag. */
+    def apply(tags : Tag*) = Tags(tags)
     /** An optional tag. */
     def apply(option : Option[Tag]) = option.getOrElse(empty)
+    /** Conditionally include a tag. The tag is only computed if the condition is true, otherwise no tag is inserted. */
+    def when(condition : Boolean, tag : => Tag) = if(condition) tag else empty
     /** An empty list of tags. Since the list is empty, it will be completely removed before the Virtual DOM is reconciled. */
     val empty : Tags = Tags(Seq())
 }
